b7ae0d6ab6d322c09065ed26cf63cb25af5bb946
Ari Kinney <akinney424@gmail.com>
2025-04-06 22:52:21 -0400

    Create AddAndRemove.java

---
+++ Arrays/AddAndRemove.java
@@ -0,0 +1,16 @@
+//Organize the code statements to modify the existing ArrayList's contents by removing the element at index 1, then adding 102 at index 1, and finally adding 100 at the beginning of the ArrayList.
+
+//Ex: If the input is 33 200 10, then the output is:
+
+//100 33 102 10
+public class AddAndRemove{
+  public staic void main(String[] args){
+    ArrayList<Integer> num = new ArrayList<Integer>();
+    int userInput;
+    int i;
+    for (i = 0; i<3 ; ){
+      
+    }
+    
+  }
+}
