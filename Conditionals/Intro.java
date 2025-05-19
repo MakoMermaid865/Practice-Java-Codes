6ba487b5d23f1e1bd5242616d9a7c5b8258f2d2a
Ari Kinney <akinney424@gmail.com>
2025-05-19 09:52:56 -0400

    Create Intro.java

---
+++ Conditionals/Intro.java
@@ -0,0 +1,6 @@
+//What are conditionals
+// why does it matter
+// Question prompt 
+// how do we use them
+
+//Run a triva game to user to test the knowledge on topic- should be a copy and past on the concept of how to code from data types code
