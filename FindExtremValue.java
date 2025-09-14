a3fc72e02d5554d0d19aa78422b514323d2ea271
Ari Kinney <akinney424@gmail.com>
2025-09-14 00:04:22 -0400

    Update FindExtremValue.java

--- FindExtremValue.java
+++ FindExtremValue.java
@@ -1,9 +1,9 @@
-//Write a for loop that iterates nine times to find the minimum value in a list of nine integer values. Within the loop:
-
-//Read an integer value from input into variable inputVal.
-//If the current iteration is the first iteration, reassign minVal with inputVal. No other values have been read yet, so the first value assigned to inputVal is the smallest integer value read by default.
-//Otherwise, if inputVal is less than minVal, reassign minVal with the value of inputVal.
-
+/**
+Write a for loop that iterates nine times to find the minimum value in a list of nine integer values. Within the loop:
+Read an integer value from input into variable inputVal.
+If the current iteration is the first iteration, reassign minVal with inputVal. No other values have been read yet, so the first value assigned to inputVal is the smallest integer value read by default.
+Otherwise, if inputVal is less than minVal, reassign minVal with the value of inputVal.
+*/
 import java.util.Scanner;
 
 public class MinValues {
